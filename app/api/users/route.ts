99eb3741758959ec76be309f4f5aa945cd235119
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-09-27 12:03:09 -0700

    added validation

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,23 +1,66 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { PrismaClient } from '@prisma/client'
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export async function GET() {
-  const users = await prisma.user.findMany()
-  return NextResponse.json(users)
+  //const users = await prisma.user.findMany();
+
+  // get all users except their password
+  const users = await prisma.user.findMany({
+    select: {
+      username: true,
+      email: true,
+      role: true,
+      timestamp: true,
+    },
+  });
+
+  return NextResponse.json(users);
 }
 
 export async function POST(req: NextRequest) {
-  const { name, email, password } = await req.json()
+  const { username, email, password } = await req.json();
 
-  if (!name || !email) {
-    return NextResponse.json({ error: 'Name, email and password are required' }, { status: 400 })
+  if (!username || !email || !password) {
+    return NextResponse.json(
+      { error: "username, email and password are required" },
+      { status: 400 }
+    );
   }
 
-  const newUser = await prisma.user.create({
-    data: { name, email, password },
-  })
+  // validate that the username and email are unique
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      OR: [{ email }, { username }],
+    },
+  });
 
-  return NextResponse.json(newUser, { status: 201 })
+  if (existingUser) {
+    return NextResponse.json(
+      { error: "username or email already exists" },
+      { status: 400 }
+    );
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: await bcrypt.hash(password, salt),
+      role: "default",
+    },
+  });
+
+  return NextResponse.json(
+    {
+      username: newUser.username,
+      email: newUser.email,
+      role: newUser.role,
+      createdAt: newUser.timestamp,
+    },
+    { status: 201 }
+  );
 }
