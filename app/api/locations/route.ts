f6006b5d097a95c5df400683c9b3aa46f80be94b
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-10-08 11:11:23 -0700

    refactored some shit

--- app/api/locations/route.ts
+++ app/api/locations/route.ts
@@ -19,38 +19,51 @@ const prisma = new PrismaClient();
 
 // Endpoint to get all locations from the database
 export async function GET() {
-    const locations = await prisma.location.findMany({
-        select: {
-            id: true,
-            name: true,
-            address: true
-        }
-    });
+    try {
+        const locations = await prisma.location.findMany({
+            select: {
+                id: true,
+                name: true,
+                address: true,
+                phoneNumber: true,
+                hasWifi: true,
+                seatingCapacity: true,
+                category: true,
+                rating: true,
+                busynessStatus: true,
+                imageWebLink: true,
+                animalFriendliness: true
+            }
+        });
 
-    return NextResponse.json(locations);
+        return NextResponse.json(locations);
+    } catch (error: any) {
+        console.log(`[ERROR]: Error in GET of api/locations/route.ts: ${error}`);
+        return NextResponse.json({ error: "Internal Server Error." }, { status: 500 });
+    }
 }
 
 // Endpoint to create a new location
 export async function POST(req: NextRequest) {
-    const body: LocationData = await req.json();
-    const requiredFields: (keyof LocationData)[] = [
-        'name',
-        'address',
-        'phoneNumber',
-        'hasWifi',
-        'seatingCapacity',
-        'category',
-        'animalFriendliness',
-    ];
-
-
-    const missingFields = requiredFields.filter(field => !body[field] === undefined);
-
-    if (missingFields.length > 0) {
-        return NextResponse.json({ error: `Missing fields: ${missingFields.join(', ')}` }, { status: 400 });
-    }
-
     try {
+        const body: LocationData = await req.json();
+        const requiredFields: (keyof LocationData)[] = [
+            'name',
+            'address',
+            'phoneNumber',
+            'hasWifi',
+            'seatingCapacity',
+            'category',
+            'animalFriendliness',
+        ];
+
+
+        const missingFields = requiredFields.filter(field => !body[field] === undefined);
+
+        if (missingFields.length > 0) {
+            return NextResponse.json({ error: `Missing fields: ${missingFields.join(', ')}` }, { status: 400 });
+        }
+
         const existingLocation = await prisma.location.findFirst({
             where: {
                 address: body.address
@@ -69,7 +82,7 @@ export async function POST(req: NextRequest) {
                 hasWifi: body.hasWifi,
                 seatingCapacity: body.seatingCapacity,
                 category: body.category,
-                AnimalFriendliness: body.animalFriendliness
+                animalFriendliness: body.animalFriendliness
             }
         });
 
@@ -78,43 +91,4 @@ export async function POST(req: NextRequest) {
         console.log(`[ERROR]: Error in POST of api/locations/route.ts: ${error}`);
         return NextResponse.json({ error: "Internal Server Error." }, { status: 500 });
     }
-
-
-
-    // const { 
-    //     name, 
-    //     address, 
-    //     phoneNumber, 
-    //     hasWifi,
-    //     seatingCapacity,
-    //     category,
-    //     rating,
-    //     busynessStatus,
-    //     ImageWebLink,
-    //     AnimalFriendliness  
-    // } = await req.json(); 
-
-    // if(!name || !address) {
-    //     return NextResponse.json({ error: "No name or address provided." }, { status: 400});
-    // }
-
-    // // check if address is already used?
-    // const existingLocation = await prisma.location.findFirst({
-    //     where: {
-    //         address
-    //     }
-    // });
-
-    // if(existingLocation) {
-    //     return NextResponse.json({ error: "Location already exists." }, { status: 400});
-    // }
-
-    // const location = await prisma.location.create({
-    //     data: {
-    //         name,
-    //         address
-    //     }
-    // });
-
-    // return NextResponse.json(location);
 }
