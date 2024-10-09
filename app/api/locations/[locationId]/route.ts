100b892ec2d8c43e081321e85ea11ce86d4d6f73
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-10-09 09:09:39 -0700

    locations refactored

--- app/api/locations/[locationId]/route.ts
+++ app/api/locations/[locationId]/route.ts
@@ -3,6 +3,46 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+export async function GET(req: NextRequest, { params }: { params: { locationId : string }}) {
+    try {
+        const locationId = params.locationId;
+
+        if(!locationId) {
+            return NextResponse.json({ error: "No locationId provided." }, { status: 400});
+        }
+
+        // get specific details for given location
+
+        const location = await prisma.location.findUnique({
+            where: {
+                id: locationId
+            },
+            include: {
+                operatingHours: {
+                    select: {
+                        day: true,
+                        timeSlots: {
+                            select: {
+                                startTime: true,
+                                endTime: true
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        if(!location) {
+            return NextResponse.json({ error: "Location not found." }, { status: 404});
+        }
+
+        return NextResponse.json(location);
+    } catch (error: any) {
+        console.log(`[ERROR]: Error in GET of api/locations/[locationId]/route.ts: ${error}`);
+        return NextResponse.json({ error: "Internal Server Error." }, { status: 500 });
+    }
+}
+
 /*
     Endpoint to get a specific location's reviews
 
