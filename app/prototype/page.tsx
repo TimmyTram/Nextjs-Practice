ca8dce3138a86549273aad116bee1476cc9ec89b
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-10-22 14:08:02 -0700

    ?

--- app/prototype/page.tsx
+++ app/prototype/page.tsx
@@ -1,7 +1,7 @@
+'use client';
 import { convertTo12HourFormat } from "../utils/utils";
 import { DayOfWeek, LocationType } from "@prisma/client";
 
-export const dynamic = 'force-dynamic'
 
 /**
  * NOTE: THIS FILE AND FOLDER IS FOR THE VERTICAL PROTOTYPE ONLY, AND WILL BE REMOVED IN THE FINAL PRODUCT.
