c22ea546a571130e90bbbd1a8ab42c46a9194d2f
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-10-22 19:09:53 -0700

    fixed?

---
+++ app/Unauthorized/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className="text-center text-red-500">
+            <h1>Unauthorized</h1>
+            <p>You are not authorized to view this page.</p>
+        </div>
+    );
+}
